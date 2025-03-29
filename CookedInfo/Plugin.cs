d068b53d1a731bf0a82ff3d80a56bdc552400e03
Bryon Bailey <bryon.r.bailey@gmail.com>
2025-03-28 22:26:50 -0400

    Update version numbers

--- CookedInfo/Plugin.cs
+++ CookedInfo/Plugin.cs
@@ -11,7 +11,7 @@ namespace CookedInfo
     {
         public const string PLUGIN_GUID = "pr0skynesis.cookedinfo";
         public const string PLUGIN_NAME = "Cooked Info";
-        public const string PLUGIN_VERSION = "1.2.0";
+        public const string PLUGIN_VERSION = "1.2.2";
 
         internal static Plugin instance;
         internal static ManualLogSource logger;
